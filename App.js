875fa1a5f9b0dac7acdefee177c31fcdeb3dadb4
danilojssantos <danilojssantos1@gmail.com>
2020-08-13 13:10:08 -0300

    carregando api

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+
+import Filmes from './src/filmes';
+
+
+import api from './src/services/api';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      filmes: []
+
+    };
+  }
+//carrega api
+  async componentDidMount(){
+    const response = await api.get('r-api/?api=filmes');
+    this.setState({
+      filmes: response.data
+    });
+  }
+  render() {
+    return(
+      <View style={styles.container}>
+        <FlatList 
+          data={this.state.filmes}
+          keyExtractor={item =>item.id.toString()}
+          renderItem={({item}) => <Filmes data={item} />}
+
+        />
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+    }
+});
+
+export default App;
